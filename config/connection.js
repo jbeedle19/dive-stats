718d7ee4aa0f9614905ffbce63dbeafd2d87de67
jbeedle19 <josh.beedle@gmail.com>
2022-04-07 17:54:42 -0400

    updated postgres to return correct data types instead of just strings

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,18 @@
 require('dotenv').config();
 
-const { Pool } = require('pg');
+const { Pool, types } = require('pg');
 
 const pool = new Pool();
+
+// This method takes an internal PostgreSQL ID (ex. 20 for the data type returned from COUNT()) and
+// returns a new value, in our case either a float or an int
+types.setTypeParser(1700, (val) => {
+    return parseFloat(val);
+});
+
+types.setTypeParser(20, (val) => {
+    return parseInt(val);
+})
 
 module.exports = {
     query: (text, params) => {
